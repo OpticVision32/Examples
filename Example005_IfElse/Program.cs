b51fa81887950baac2ce36e7b4a29032372f9a4f
Igor Zhidkov <stork32@mail.ru>
2022-11-28 13:18:30 +0300

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "сергей")
+{
+    Console.Write("Ура! Это же Сергей! ");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.Write(username + "!");
+}
+
+
+
+
+
+
+
+
+
+
