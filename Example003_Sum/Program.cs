079880b37db1a224acf526247d8934a7d6ddc314
Igor Zhidkov <stork32@mail.ru>
2022-11-28 12:41:52 +0300

    add random number

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,5 @@
-﻿Console.Write("Введите число: ");
-// Для того чтобы ввести в консоли в переменную целове число, конвертировали значение, т.к команжа ReadLine позволяет вносить только string.
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int B = Convert.ToInt32(Console.ReadLine());
-Console.Write("Сумма чисел равна : ");
-Console.Write(A + B);
+﻿int A = new Random().Next(1,10);
+Console.WriteLine(A);
+int B = new Random().Next(1,10);
+Console.WriteLine(B);
+Console.WriteLine(A + B);
