289d7df836249c7f63832c2269c5d727edc5c259
Igor Zhidkov <stork32@mail.ru>
2022-11-28 11:28:30 +0300

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите ваше имя: ");
+// Присвоение переменной с типом данным "Строка" того, что мы написали в консоль
+string username = Console.ReadLine();
+// Вывод текста и переменной
+Console.Write("Привет, ");
+Console.Write(username);
