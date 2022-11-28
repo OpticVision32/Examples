722fc64258713779d067805374b5d298c9ecb722
Igor Zhidkov <stork32@mail.ru>
2022-11-28 12:06:29 +0300

    add Example 003 Summ

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+// Для того чтобы ввести в консоли в переменную целове число, конвертировали значение, т.к команжа ReadLine позволяет вносить только string.
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма чисел равна : ");
+Console.Write(A + B);
