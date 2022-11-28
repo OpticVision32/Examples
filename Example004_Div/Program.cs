88b663af5a501ef26b4f737fe1dad8b8fc7b2ab2
Igor Zhidkov <stork32@mail.ru>
2022-11-28 12:25:34 +0300

    add Example 004 Division

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+// Для того чтобы ввести в консоли в переменную целое число, конвертировали значение, т.к команда ReadLine позволяет вносить только string.
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double B = Convert.ToDouble(Console.ReadLine());
+Console.Write("Частное равно: ");
+Console.Write(A / B);
