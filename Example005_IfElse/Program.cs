7ae9a36c8ef41bdff55f41c89d1aa4383b48f4a6
Igor Zhidkov <stork32@mail.ru>
2022-12-02 12:25:23 +0300

    fix Ex005

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
+string username = Console.ReadLine() ?? "";
 if (username.ToLower() == "сергей")
 {
     Console.Write("Ура! Это же Сергей! ");
